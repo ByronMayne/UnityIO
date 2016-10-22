dcd301447d6cfd2d3cd4cab9afb045d7fd8ad293
Byron Mayne <ByronMayne@gmail.com>
2016-10-22 16:00:22 -0400

    Updated the IsEmpty interface and fixed some unit tests.

--- Assets/UnityIO/Editor/Unit Tests/ConditionalProgressTests.cs
+++ Assets/UnityIO/Editor/Unit Tests/ConditionalProgressTests.cs
@@ -1,9 +1,8 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using UnityEngine;
+using UnityEditor;
 using UnityIO;
+using Object = UnityEngine.Object;
 
 public class ConditionalProgressTests
 {
@@ -20,20 +19,41 @@ public class ConditionalProgressTests
         IO.Root.IfDirectoryExists("Conditional Progress").CreateDirectory("Sub Directory");
         // It should not exists since we created the parent directory
         Assert.True(IO.Root.DirectoryExists("Conditional Progress/Sub Directory"));
+        // Cleanup
+        IO.Root.IfDirectoryExists("Conditional Progress").Delete();
     }
 
     [Test]
-    public void ConditionFolderNested()
+    public void EmptyCheck()
     {
-        // Only create Sub Directory if Conditional Progress exists.
-        IO.Root.IfDirectoryExists("Conditional Progress").CreateDirectory("Sub Directory");
-        // It should not exists
-        Assert.False(IO.Root.DirectoryExists("Conditional Progress/Sub Directory"));
-        // Then really create it
-        IO.Root.CreateDirectory("Conditional Progress");
-        // Then try conditional again
-        IO.Root.IfDirectoryExists("Conditional Progress").CreateDirectory("Sub Directory");
-        // It should not exists since we created the parent directory
-        Assert.True(IO.Root.DirectoryExists("Conditional Progress/Sub Directory"));
+        // Create a new directory
+        IO.Root.CreateDirectory("If Empty");
+        // Check if it's empty
+        Assert.True(IO.Root["If Empty"].IsEmpty(assetsOnly: false), "This should empty");
+        // Add a sub folder
+        IO.Root["If Empty"].CreateDirectory("Sub Directory");
+        // This should be false since we say we want to include sub folders.
+        Assert.False(IO.Root["If Empty"].IsEmpty(assetsOnly: false), "This should have passed because we have one folder.");
+        // This should be false because we only care about Assets. 
+        Assert.False(IO.Root["If Empty"].IsEmpty(assetsOnly: true), "This should empty");
+        // Cleanup
+        IO.Root["If Empty"].Delete();
+    }
+
+    [Test]
+    public void ConditionEmptyTest()
+    {
+        // Create a new directory
+        IO.Root.CreateDirectory("Conditional Empty").CreateDirectory("Sub Directory");
+        // Destroy it if it's empty (it's not).
+        IO.Root["Conditional Empty"].IfEmpty(assetsOnly: false).Delete();
+        // Check if it was deleted (it should not have been).
+        Assert.True(IO.Root.DirectoryExists("Conditional Empty"), "The folder should not have been deleted");
+        // Delete the next level instead this should work.
+        IO.Root["Conditional Empty/Sub Directory"].IfEmpty(assetsOnly: false).Delete();
+
+        // Check if it was deleted (it should have been).
+        //Assert.False(IO.Root.DirectoryExists("Conditional Empty/Sub Directory"), "The folder should have been deleted");
+
     }
 }
--- Assets/UnityIO/Interfaces/IDirectory.cs
+++ Assets/UnityIO/Interfaces/IDirectory.cs
@@ -5,13 +5,26 @@ namespace UnityIO.Interfaces
     public interface IDirectory
     {
         void Delete();
+        void Duplicate();
+        void Duplicate(string newName);
+
+        void Move(string newPath);
+        void Rename(string newName);
+
         void DeleteSubDirectory(string directroyName);
         bool DirectoryExists(string directoryName);
+
+        bool IsEmpty(bool assetsOnly);
 
         IDirectory this[string name] { get; }
         IDirectory GetDirectory(string name);
         IDirectory CreateDirectory(string name);
+
+        // Conditionals 
         IDirectory IfDirectoryExists(string name);
+        IFile IfFileExists(string name);
+        IDirectory IfEmpty(bool assetsOnly);
+        IDirectory IfNotEmpty(bool assetsOnly);
 
         IFile GetFiles();
         IFile GetFiles(bool recursive);
@@ -21,6 +34,6 @@ namespace UnityIO.Interfaces
         IFile GetFiles<T>(bool recursive) where T : UnityEngine.Object;
         IFile GetFiles<T>(string filter) where T : UnityEngine.Object;
         IFile GetFiles<T>(string filter, bool recursive);
-        IFile IfFileExists(string name);
+ 
     }
 }
